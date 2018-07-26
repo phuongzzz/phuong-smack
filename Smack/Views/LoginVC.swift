515366c955cc07b3a388233f76192e4e34c2a8f9
phuongzzz <yesterdayoncemore23@gmail.com>
2018-07-27 00:19:01 +0700

    perform close button for login vc

---
+++ Smack/Views/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by phuongzzz on 7/27/18.
+//  Copyright © 2018 phuongzzz. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
