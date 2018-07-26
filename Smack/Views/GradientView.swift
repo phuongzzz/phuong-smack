443ebd9a76deefd38e8f9e1a773299531692aa3b
phuongzzz <yesterdayoncemore23@gmail.com>
2018-07-26 21:20:00 +0700

    create new custom gradient view layer

---
+++ Smack/Views/GradientView.swift
@@ -0,0 +1,46 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by phuongzzz on 7/26/18.
+//  Copyright © 2018 phuongzzz. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor] //Why need cgColor here?
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
