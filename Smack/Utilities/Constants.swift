29c426cd1d2f96e18f8ee541075c81ea274e51f6
phuongzzz <yesterdayoncemore23@gmail.com>
2018-07-28 23:31:55 +0700

    part 1 of auth-service done

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> () // TODO: WHAT DOES IT MEANS?
+
+// URL constants
+let BASE_URL = "https://phuong-chat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// Auth Services
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
